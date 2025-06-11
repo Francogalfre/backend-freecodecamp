fbdd3b4209d8e2e35cba650e2b730f5bb94bfa80
Franco Galfre <francogalfre2007@gmail.com>
2025-06-11 17:23:57 -0300

    Node Challenge 2: (error) Serves Hello Express in the / root

--- myApp.js
+++ myApp.js
@@ -3,8 +3,8 @@ let express = require("express");
 let app = express();
 
 app.get("/", (req, res) => {
-  console.log("Hello World");
-  res.send("Hello World");
+  console.log("Hello Express");
+  res.send("Hello Express");
 });
 
 module.exports = app;
