72a91ddf98fce67e4edb0dd79d64df4c752853b3
Franco Galfre <francogalfre2007@gmail.com>
2025-06-11 17:38:41 -0300

    Node Challenge 4: Add CSS Styles File in the main root

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ let express = require("express");
 
 let app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
   console.log("Hello Express");
 
