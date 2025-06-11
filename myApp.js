626a993318d600a09a5380f9b3ea08b7c6720b2c
Franco Galfre <francogalfre2007@gmail.com>
2025-06-11 17:21:10 -0300

    Node Challenge 2: Serves Hello World in the / root

--- myApp.js
+++ myApp.js
@@ -3,9 +3,8 @@ let express = require("express");
 let app = express();
 
 app.get("/", (req, res) => {
+  console.log("Hello World");
   res.send("Hello World");
 });
-
-console.log("Hello World");
 
 module.exports = app;
