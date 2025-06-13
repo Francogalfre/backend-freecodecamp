67a248af729eca6b66f67f16f680eb9ad2f09292
Franco Galfre <francogalfre2007@gmail.com>
2025-06-12 22:48:55 -0300

    Node Challenge 6: (Adding console log for debug) Using the .env file

--- myApp.js
+++ myApp.js
@@ -7,11 +7,13 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   console.log("Hello Express");
-
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/json", (req, res) => {
+  // *** Trying to find the error ***
+  console.log("Valor de MESSAGE_STYLE en Render:", process.env.MESSAGE_STYLE);
+
   const textStyle = process.env.MESSAGE_STYLE;
 
   if (textStyle == "uppercase") {
