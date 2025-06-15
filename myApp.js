0cdb03fcfbe48af254a7d4e20fe9172141bc239f
Franco Galfre <francogalfre2007@gmail.com>
2025-06-14 23:35:29 -0300

    Node Challenge 8: Chain Middleware for Time Server

--- myApp.js
+++ myApp.js
@@ -5,17 +5,29 @@ let app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.use("/", (req, res, next) => {
+app.use("/", (req, _res, next) => {
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
 });
 
-app.get("/", (req, res) => {
+app.use(
+  "/now",
+  (req, _res, next) => {
+    const time = new Date().toString();
+    req.time = time;
+    next();
+  },
+  function (req, res) {
+    res.json({ time: req.time });
+  }
+);
+
+app.get("/", (_req, res) => {
   console.log("Hello Express");
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/json", (req, res) => {
+app.get("/json", (_req, res) => {
   const textStyle = process.env.MESSAGE_STYLE;
 
   if (textStyle == "uppercase") {
