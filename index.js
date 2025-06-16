9eb56abd6f4ae626ebf62d15fbecdc2282ef2e90
Franco Galfre <francogalfre2007@gmail.com>
2025-06-16 12:11:32 -0300

    Challenge 2: Second Try

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var express = require("express");
-var app = express();
+import express from "express";
+import cors from "cors";
 
-var cors = require("cors");
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
 app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use(express.static("public"));
@@ -11,20 +18,36 @@ app.get("/", function (req, res) {
 });
 
 app.get("/api/:date?", (req, res) => {
-  const paramsDate = req.params.date;
-  const date = new Date(Number(paramsDate));
+  const dateParams = req.params.date;
 
-  if (date.toString() === "Invalid Date") {
-    return res.json({ error: "Invalid Date" });
-  }
-
-  if (!paramsDate) {
-    res.json({ unix: new Date().getTime(), utc: new Date().toUTCString() });
+  if (dateParams == undefined) {
+    res.json({
+      unix: Date.now(),
+      utc: new Date().toUTCString(),
+    });
   } else {
-    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+    let date;
+
+    if (!isNaN(dateParams)) {
+      date = new Date(parseInt(dateParams));
+    } else {
+      date = new Date(dateParams);
+    }
+
+    if (date.toString() == "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } else {
+      const unixDate = date.getTime();
+      const utcDate = date.toUTCString();
+
+      res.json({
+        unix: unixDate,
+        utc: utcDate,
+      });
+    }
   }
 });
 
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
